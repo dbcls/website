a87994fa57031ab9a8fae579079d5c9300d73fc4
平原麗夏 <hiraharareika@naganoakiraotto-no-iMac.local>
2018-06-07 12:08:49 +0900

    add favicon

--- _site/script/default.js
+++ _site/script/default.js
@@ -1,5 +1,7 @@
 var script = document.createElement('script')
 var script_sticky = document.createElement('script')
+var link_favicon = document.createElement('link')
+
 var tags = {
   'public_relations': '広報',
   'services': 'サービス',
@@ -10,8 +12,12 @@ var tags = {
 
 script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js')
 script_sticky.setAttribute('src', '/script/stickyfill.min.js')
+link_favicon.setAttribute('rel', 'icon')
+link_favicon.setAttribute('type', 'image/png')
+link_favicon.setAttribute('href', '/img/favicon.png')
 document.head.appendChild(script)
 document.head.appendChild(script_sticky)
+document.head.appendChild(link_favicon)
 var initialize = {
   'index': function() {
     $('.news__individual-wrapper').css('display', 'block')
