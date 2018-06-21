c642c054847ece31852107b21c91f0d6a046bbda
reika0717 <reikahirahara@gmail.com>
2018-06-21 15:20:34 +0900

    replace top images to beautify

---
+++ _site/common-header-footer/common-header/script/common-header.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script')
+script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js')
+document.head.appendChild(script)
+
+setTimeout(function(){
+  $('body').prepend('<header>')
+  $('header').addClass('dbcls-common-header')
+  $('header').load('common-header.html')
+  $('body').css('padding-top', '24px')
+}, 100)
