d5635b33d8fae06bce1f79dcc52e949a2bcb587d
永野 朗夫 <nagano@me.com>
2020-02-10 20:22:40 +0900

    modify common header to hide when app boots

--- DBCLS-common-header-footer-dev/common-header-and-footer/script/common-header-and-footer-min.js
+++ DBCLS-common-header-footer-dev/common-header-and-footer/script/common-header-and-footer-min.js
@@ -19,8 +19,8 @@
       en: '<div class="footer__common"><div class="footer__wrapper"><div class="footer__wrapper__contents"><div class="footer__info footer__info-en"><div class="footer__organism"><img src="@@path@@/DBCLS-common-header-footer/common-header-and-footer/img/logo_dbcls.svg" alt="" class="footer__logo"><div class="footer__organism-text"><p class="footer__organism-main">Database Center for Life Science</p><p class="footer__organism-sub">Joint Support-Center for Data Science Research</p><p class="footer__organism-sub">Research Organization of Information and Systems</p></div></div><div class="footer__address"><div class="footer-title">Address</div><ul class="address__list"><li><p>Kashiwa-Lab.</p><p>Univ. of Tokyo Kashiwa-no-ha Campus Station Satellite 6F.</p><p>178-4-4 Wakashiba, Kashiwa, Chiba 277-0871, JAPAN</p><p>TEL +81-4-7135-5508<br>FAX +81-4-7135-5534</p></li><li><p>Mishima-Lab.</p><p>Center for Information Biology W412, </p><p>National Institute of Genetics</p><p>Yata 1111, Mishima, Shizuoka 411-8540, JAPAN</p></li></ul></div></div></div></div><div class="footer__lisence">@@license@@</div></div>'
     },
     simple: {
-      ja: '<div class="footer__common"><div class="footer__wrapper"><p class="footer__logo">Database Center for Life Science</p><ul class="footer__links"><li><a href="http://dbcls.rois.ac.jp/contact.html">Contact</a></li><li><a href="https://dbcls.rois.ac.jp/policy.html">Site policy</a></li></ul><p>&copy; @@year@@ DBCLS</p>@@license@@</div></div>',
-      en: '<div class="footer__common"><div class="footer__wrapper"><p class="footer__logo">Database Center for Life Science</p><ul class="footer__links"><li><a href="http://dbcls.rois.ac.jp/contact.html">Contact</a></li><li><a href="https://dbcls.rois.ac.jp/policy-en.html">Site policy</a></li></ul><p>&copy; @@year@@ DBCLS</p>@@license@@</div></div>'
+      ja: '<div class="footer__common"><div class="footer__wrapper"><p class="footer__logo">Database Center for Life Science</p><ul class="footer__links"><li><a href="https://dbcls.rois.ac.jp/contact.html">Contact</a></li><li><a href="https://dbcls.rois.ac.jp/policy.html">Site policy</a></li></ul><p>&copy; @@year@@ DBCLS</p>@@license@@</div></div>',
+      en: '<div class="footer__common"><div class="footer__wrapper"><p class="footer__logo">Database Center for Life Science</p><ul class="footer__links"><li><a href="https://dbcls.rois.ac.jp/contact.html">Contact</a></li><li><a href="https://dbcls.rois.ac.jp/policy-en.html">Site policy</a></li></ul><p>&copy; @@year@@ DBCLS</p>@@license@@</div></div>'
     }
   }
   var LICENSE_HTML = {
@@ -73,7 +73,7 @@
       .attr('id', 'dbcls-common-header')
       .addClass(menuType)
       .html('\
-        <nav class="gnav">\
+        <nav class="gnav" style="display: none">\
           <ul class="pc_nav">\
             <li>\
               <a href="https://dbcls.rois.ac.jp/index.html" class="dbcls-logo"></a>\
@@ -93,7 +93,7 @@
           </ul>\
           <img src="https://dbcls.rois.ac.jp/DBCLS-common-header-footer/common-header-and-footer/img/menu.svg" class="hamburger">\
         </nav>\
-        <ul class="sp_nav">\
+        <ul class="sp_nav" style="display: none">\
           <li>\
             <a href="https://dbcls.rois.ac.jp/research.html">Research</a>\
           </li>\
@@ -137,11 +137,10 @@
     $j('body').append('<div id="dbcls-common-footer">')
     var $footer = $j('#dbcls-common-footer')
     var current_lang = $j('html').attr('lang')
-    //var footerDesign = dataset.footerDesign ? dataset.footerDesign : 'full'
     var footerDesign = 'simple'
     var showFooterLicense = dataset.showFooterLicense ? dataset.showFooterLicense : 'false'
     var showFooterLinks = dataset.showFooterLinks ? dataset.showFooterLinks : 'false'
-    var license = dataset.pageType ? dataset.pageType : 'none'
+    var license = dataset.licenseType ? dataset.licenseType : 'none'
     var year = dataset.year ? dataset.year : new Date().getFullYear()
     current_lang = current_lang ? current_lang : 'ja'
     var footerHtml = FOOTER_HTML[footerDesign][current_lang]
