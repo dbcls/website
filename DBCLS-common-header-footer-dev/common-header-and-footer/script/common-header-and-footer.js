be281db0843b8090feb31af1eaf4c91e87980ed2
永野 朗夫 <nagano@me.com>
2021-07-06 18:42:19 +0900

    resolve common header-footer style issue

--- DBCLS-common-header-footer-dev/common-header-and-footer/script/common-header-and-footer.js
+++ DBCLS-common-header-footer-dev/common-header-and-footer/script/common-header-and-footer.js
@@ -64,7 +64,7 @@
         'position': 'fixed',
         'top': 0,
         'left': 0,
-        'width': '100vw',
+        'width': '100%',
         'height': '24px',
         'background': background
       })
