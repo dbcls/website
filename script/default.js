5784217828b8af49f25b4ef1472a2ca7e9c0db11
Marco Chan <marcochan94@hotmail.com>
2023-01-31 12:45:24 +0900

    combine same logic in access.js&events.js

--- script/default.js
+++ script/default.js
@@ -54,6 +54,37 @@ const addColorTags = (tags) => {
   }
 };
 
+const leftSidebarAutoScroll = (idName) => {
+  /***左サイドバーの動作ここから***/
+  //変動要素: main__contents-"event"
+  //main一つ目セクションの高さの取得
+  var client_h = document.getElementById(idName).clientHeight;
+
+  //クリックでactiveが切り替わる
+  $('ul.sub__navigation-wrapper li').on('click', function () {
+    $('ul.sub__navigation-wrapper li').removeClass('active');
+    $(this).addClass('active');
+  });
+  //取得したある高さまで、移動
+  $('.sub_2').on('click', function () {
+    window.scrollTo(0, client_h);
+  });
+  $('.sub_1').on('click', function () {
+    window.scrollTo(0, 0);
+  });
+  //スクロールでactiveが切り替わる
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > client_h) {
+      $('ul.sub__navigation-wrapper li.sub_1').removeClass('active');
+      $('ul.sub__navigation-wrapper li.sub_2').addClass('active');
+    } else if ($(this).scrollTop() < client_h) {
+      $('ul.sub__navigation-wrapper li.sub_2').removeClass('active');
+      $('ul.sub__navigation-wrapper li.sub_1').addClass('active');
+    }
+  });
+  /***左サイドバーの動作ここまで***/
+};
+
 var initialize = {
   index: function () {
     addColorTags(tags);
@@ -77,9 +108,12 @@ var initialize = {
   services: function () {},
   events: function () {
     addColorTags(tags);
+    leftSidebarAutoScroll('main__contents-event');
   },
   members: function () {},
-  access: function () {},
+  access: function () {
+    leftSidebarAutoScroll('main__contents-kashiwa');
+  },
   contact: function () {},
 };
 
--- script/events.js
+++ script/events.js
@@ -62,33 +62,4 @@ script.addEventListener('load', function () {
       $('.section-wrapper').append(elements);
     },
   });
-
-  /***左サイドバーの動作ここから***/
-  //変動要素: main__contents-"event"
-  //mein一つ目セクションの高さの取得
-  var client_h = document.getElementById('main__contents-event').clientHeight;
-
-  //クリックでactiveが切り替わる
-  $('ul.sub__navigation-wrapper li').on('click', function () {
-    $('ul.sub__navigation-wrapper li').removeClass('active');
-    $(this).addClass('active');
-  });
-  //取得したある高さまで、移動
-  $('.sub_2').on('click', function () {
-    window.scrollTo(0, client_h);
-  });
-  $('.sub_1').on('click', function () {
-    window.scrollTo(0, 0);
-  });
-  //==>スクロールでactiveが切り替わる
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > client_h) {
-      $('ul.sub__navigation-wrapper li.sub_1').removeClass('active');
-      $('ul.sub__navigation-wrapper li.sub_2').addClass('active');
-    } else if ($(this).scrollTop() < client_h) {
-      $('ul.sub__navigation-wrapper li.sub_2').removeClass('active');
-      $('ul.sub__navigation-wrapper li.sub_1').addClass('active');
-    }
-  });
-  /***左サイドバーの動作ここまで***/
 });
