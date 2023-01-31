52d20c20baf322826a86ab42e0c42d73326a64e2
Marco Chan <marcochan94@hotmail.com>
2023-01-31 12:47:43 +0900

    delete unused script in access

--- script/access.js
+++
@@ -1,8 +0,0 @@
-var script = document.createElement('script');
-
-script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
-document.head.appendChild(script);
-
-script.addEventListener('load', function () {
-
-});
