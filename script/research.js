8a5273faa925f3f88c335fde6335d6cd2cd53f43
Marco Chan <marcochan94@hotmail.com>
2023-02-06 17:51:08 +0900

    add Details button in Research page

--- script/research.js
+++ script/research.js
@@ -1,4 +1,14 @@
 var script = document.createElement('script');
+var material_icons = document.createElement('link');
+
+script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
+script_sticky.setAttribute('src', '/script/stickyfill.min.js');
+material_icons.setAttribute('rel', 'stylesheet');
+material_icons.setAttribute(
+  'href',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+);
+document.head.appendChild(material_icons);
 
 script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
 document.head.appendChild(script);
