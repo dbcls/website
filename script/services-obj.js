be361f6e4529ddc8b7bebb248b525d630ecb45c6
Marco Chan <marcochan94@hotmail.com>
2023-02-08 21:03:06 +0900

    refactor js to reference obj json

---
+++ script/services-obj.js
@@ -0,0 +1,500 @@
+var script = document.createElement('script');
+
+script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
+document.head.appendChild(script);
+
+script.addEventListener('load', function () {
+  var url = window.location;
+  var path = url.href.split('/');
+  var file_name = path.pop();
+  if (file_name.match(/#[\w_]/)) {
+    file_name = file_name.replace('services.html#', '');
+    file_name = file_name.replace('services-en.html#', '');
+    $('.facet_section').addClass('off');
+    displayRepos(file_name);
+  } else {
+    servicesFrontDisplay();
+  }
+  function servicesFrontDisplay() {
+    $.ajax({
+      url: '../json/services-object.json',
+      dataType: 'json',
+      async: true,
+      success: function (data) {
+        var elementArray = data;
+        //column1に"Y"のあるrowをとってくる
+        var symbolYList = elementArray.filter((YList) => {
+          return YList['掲載'] === 'Y';
+        });
+        var element = '';
+
+        function getClassName(num) {
+          var tagName = [];
+          if (symbolYList[num]['Category_1'] === 'Y') {
+            tagName.push('database-integration');
+          }
+          if (symbolYList[num]['Category_2'] === 'Y') {
+            tagName.push('materials');
+          }
+          if (symbolYList[num]['Category_3'] === 'Y') {
+            tagName.push('genome');
+          }
+          if (symbolYList[num]['Category_4'] === 'Y') {
+            tagName.push('gene');
+          }
+          if (symbolYList[num]['Category_5'] === 'Y') {
+            tagName.push('gene-expression');
+          }
+          if (symbolYList[num]['Category_6'] === 'Y') {
+            tagName.push('NGS');
+          }
+          if (symbolYList[num]['Category_7'] === 'Y') {
+            tagName.push('disease');
+          }
+          if (symbolYList[num]['Category_8'] === 'Y') {
+            tagName.push('natural-language-processing');
+          }
+          if (symbolYList[num]['Category_9'] === 'Y') {
+            tagName.push('SPARQL');
+          }
+          if (symbolYList[num]['Category_10'] === 'Y') {
+            tagName.push('RDF-creation');
+          }
+          if (symbolYList[num]['User_1'] === 'Y') {
+            tagName.push('biologist');
+          }
+          if (symbolYList[num]['User_2'] === 'Y') {
+            tagName.push('application');
+          }
+          if (symbolYList[num]['User_3'] === 'Y') {
+            tagName.push('data-scientist');
+          }
+          if (symbolYList[num]['User_4'] === 'Y') {
+            tagName.push('provider');
+          }
+          return tagName;
+        }
+
+        var tagMapping = {
+          'database-integration': {
+            ja: 'データベース統合',
+            en: 'Database integration',
+          },
+          materials: {
+            ja: '教材・資料',
+            en: 'Materials',
+          },
+          genome: {
+            ja: 'ゲノム',
+            en: 'Genome',
+          },
+          gene: {
+            ja: '遺伝子',
+            en: 'Gene',
+          },
+          'gene-expression': {
+            ja: '遺伝子発現',
+            en: 'Gene expression',
+          },
+          NGS: {
+            ja: 'NGS',
+            en: 'NGS',
+          },
+          disease: {
+            ja: '疾患',
+            en: 'Disease',
+          },
+          'natural-language-processing': {
+            ja: '自然言語処理',
+            en: 'Natural language processing',
+          },
+          SPARQL: {
+            ja: 'SPARQL検索',
+            en: 'SPARQL Search',
+          },
+          'RDF-creation': {
+            ja: 'RDF作成',
+            en: 'RDF creation',
+          },
+          biologist: {
+            ja: 'データベース利用者',
+            en: 'Database user',
+          },
+          application: {
+            ja: 'アプリケーション開発者',
+            en: 'Database application developer',
+          },
+          'data-scientist': {
+            ja: '大規模データ解析者',
+            en: 'Data scientist',
+          },
+          provider: {
+            ja: ' データ所有者',
+            en: 'Data provider',
+          },
+        };
+
+        //file名の取得
+        for (var i = 0; i < symbolYList.length; i++) {
+          var tagArray = getClassName(i);
+          var tagName = tagArray.join(' ');
+
+          function addTagLine(array, lang) {
+            var categoryTag = '<div class="tag_wrapper">';
+            if (lang === 'ja') {
+              for (var j = 0; j < array.length; j++) {
+                var category_name = array[j];
+                let user = '';
+                if (
+                  category_name === 'biologist' ||
+                  category_name === 'application' ||
+                  category_name === 'data-scientist' ||
+                  category_name === 'provider'
+                ) {
+                  user = 'user';
+                }
+                categoryTag +=
+                  '<div class="service_category card ' +
+                  user +
+                  ' tag_element ' +
+                  array[j] +
+                  '">' +
+                  tagMapping[category_name].ja +
+                  '</div>';
+              }
+            } else if (lang === 'en') {
+              for (var j = 0; j < array.length; j++) {
+                var category_name = array[j];
+                let user = '';
+                if (
+                  category_name === 'biologist' ||
+                  category_name === 'application' ||
+                  category_name === 'data-scientist' ||
+                  category_name === 'provider'
+                ) {
+                  user = 'user';
+                }
+                categoryTag +=
+                  '<div class="service_category card ' +
+                  user +
+                  ' tag_element ' +
+                  array[j] +
+                  '">' +
+                  tagMapping[category_name].en +
+                  '</div>';
+              }
+            }
+            categoryTag += '</div>';
+            return categoryTag;
+          }
+
+          var service_name_hash = symbolYList[i]['services_name_en'];
+          service_name_hash = service_name_hash.replace(/ /g, '_');
+          if (service_name_hash.indexOf('/') !== -1) {
+            service_name_hash = encodeURIComponent(service_name_hash);
+          }
+
+          var lang = $('html').attr('lang');
+          if (lang === 'ja') {
+            element +=
+              '<article class="article__section contener-type-box mix ' +
+              tagName +
+              '">' +
+              '<div id="repos_image0" class="repos_image">' +
+              '<a href="' +
+              symbolYList[i][URL] +
+              '" target="_blank">' +
+              '<img src="./img/service_assets/' +
+              symbolYList[i]["画像"] +
+              '" alt="' +
+              symbolYList[i]['services_name_en'] +
+              '" class="object-fit-img img_services" /></a></div>' +
+              '<div id="repos_name' +
+              i +
+              '" class="repos_name">' +
+              '<p class="name name_ja" id="' +
+              symbolYList[i]['services_name_en'] +
+              '">' +
+              symbolYList[i]['services_name_ja'] +
+              '</p>' +
+              '<div class="keyword"><p>' +
+              symbolYList[i]['explanation_ja'] +
+              '</p></div>' +
+              addTagLine(tagArray, 'ja') +
+              '<div class="btn-box">' +
+              '<a class="page_btn more_btn" target="_blank" href="#' +
+              service_name_hash +
+              '">' +
+              '詳細' +
+              '</a>' +
+              '<a href="' +
+              symbolYList[i][URL] +
+              '" class="page_btn access_btn" target="_blank">アクセス</a>' +
+              '</div></div>';
+          } else if (lang === 'en') {
+            element +=
+              '<article class="article__section contener-type-box mix ' +
+              tagName +
+              '">' +
+              '<div id="repos_image0" class="repos_image">' +
+              '<a href="' +
+              symbolYList[i][URL] +
+              '" target="_blank">' +
+              '<img src="./img/service_assets/' +
+              symbolYList[i]["画像"] +
+              '" alt="' +
+              symbolYList[i]['services_name_en'] +
+              '" class="object-fit-img img_services" /></a></div>' +
+              '<div id="repos_name' +
+              i +
+              '" class="repos_name">' +
+              '<p class="name name_en" id="' +
+              symbolYList[i]['services_name_en'] +
+              '">' +
+              symbolYList[i]['services_name_en'] +
+              '</p>' +
+              '<div class="keyword"><p>' +
+              symbolYList[i]['explanation_en'] +
+              '</p></div>' +
+              addTagLine(tagArray, 'en') +
+              '<div class="btn-box">' +
+              '<a class="page_btn more_btn" target="_blank" href="#' +
+              service_name_hash +
+              '">' +
+              'more' +
+              '</a>' +
+              '<a href="' +
+              symbolYList[i][URL] +
+              '" class="page_btn access_btn" target="_blank">Access</a>' +
+              '</div></div>';
+          }
+
+          element += '</article>';
+        }
+        $('#service_list').append(element);
+        var containerEl = document.querySelector('.service__wrapper');
+        mixitup(containerEl, {
+          multifilter: {
+            enable: true,
+          },
+          debug: {
+            enable: true,
+          },
+          callbacks: {
+            onMixClick: function (state, originalEvent) {
+              const e = originalEvent;
+              const isCategory = e.target.classList.contains('category');
+              const buttonType = () => (isCategory ? 'category' : 'user');
+              const isAllButton = e.target.classList.contains('all');
+              const isActiveButton = e.target.classList.contains(
+                'mixitup-control-active'
+              );
+              const currentUrl = new URL(window.location.href);
+              const searchParams = new URLSearchParams(currentUrl.search);
+              const buttonName = e.target.dataset.toggle?.substr(1);
+              const urlParamsHandler = (paramKey) => {
+                const paramValue = searchParams.get(paramKey);
+                const paramValueArray = () => {
+                  return paramValue !== null ? paramValue.split(',') : [];
+                };
+                const addParamValue = () => {
+                  let newParamValue = paramValue + ',' + buttonName;
+                  searchParams.set(paramKey, newParamValue);
+                };
+                const deleteParamValue = () => {
+                  const copy = [...paramValueArray()];
+                  const targetIndex = copy.indexOf(buttonName);
+                  copy.splice(targetIndex, 1);
+                  searchParams.set(paramKey, copy.toString());
+                };
+                if (isAllButton) {
+                  searchParams.delete(paramKey);
+                } else {
+                  if (paramValueArray().length === 0) {
+                    searchParams.append(paramKey, buttonName);
+                  } else {
+                    if (paramValueArray().includes(buttonName)) {
+                      if (isActiveButton) {
+                        paramValueArray().length === 1
+                          ? searchParams.delete(paramKey)
+                          : deleteParamValue();
+                      }
+                    } else {
+                      addParamValue();
+                    }
+                  }
+                }
+                currentUrl.search = searchParams.toString();
+                let newUrl = currentUrl.href;
+                window.history.pushState({}, '', newUrl);
+              };
+              urlParamsHandler(buttonType());
+            },
+            onMixStart: function (state, futureState) {
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+              });
+              const currentUrl = new URL(window.location.href);
+              const searchParams = new URLSearchParams(currentUrl.search);
+              const hasCategory = searchParams.has('category');
+              const hasUser = searchParams.has('user');
+              const allAllButtons = '[data-filter=".mix"]';
+              $(allAllButtons).removeClass('active');
+              if (currentUrl.search === '') {
+                $(allAllButtons).addClass('active');
+              } else if (!hasCategory && hasUser) {
+                $('button.category.all').addClass('active');
+              } else if (hasCategory && !hasUser) {
+                $('button.user.all').addClass('active');
+              }
+            },
+          },
+        });
+        // URL parameter を応じてユーザボタンを押す
+        function clickButtonsByUrlParams() {
+          const currentUrl = new URL(window.location.href);
+          const searchParams = new URLSearchParams(currentUrl.search);
+          for (const values of searchParams.values()) {
+            for (const value of values.split(',')) {
+              var selector = 'button.tag_element.' + value;
+              $(selector).click();
+            }
+          }
+        }
+        clickButtonsByUrlParams();
+      },
+    });
+  }
+
+  //詳細ボタンクリックでREADME表示
+  // $(document).on('click', '.more_btn', function () {
+  //   var service_name = $(this)
+  //     .parent()
+  //     .siblings(".name")
+  //     .attr("id");
+  //   service_name = service_name.replace(/ /g, '_')
+  //   if(service_name.indexOf('/') !== -1) {
+  //     service_name = encodeURIComponent(service_name)
+  //   }
+  //   location.hash = service_name
+  // })
+
+  //リポジトリ個別ページ
+  function displayRepos(repos_name) {
+    $('title').text(
+      `${decodeURIComponent(repos_name.replace(/\_/g, ' '))} | DBCLS`
+    );
+
+    $('html,body').scrollTop(0);
+    var md_data = '';
+    $.ajax({
+      type: 'get',
+      url: '../json/services.json',
+      dataType: 'json',
+    }).done(function (data) {
+      var services_array = data;
+      var services_array_Y = services_array.filter((services_array) => {
+        return services_array['掲載'] === 'Y';
+      });
+
+      function checkCore(name) {
+        var coreName = '';
+        var sameTypeServices = [];
+        for (var i = 0; i < services_array_Y.length; i++) {
+          var service = services_array_Y[i]['services_name_en'];
+          var core_service = services_array_Y[i]['services_name_ja'];
+          name = name.replace(/_/g, ' ');
+          if (service === name) {
+            coreName = services_array_Y[i]['services_name_ja'];
+          }
+        }
+        for (var i = 0; i < services_array_Y.length; i++) {
+          var service = services_array_Y[i]['services_name_en'];
+          var core_service = services_array_Y[i]['services_name_ja'];
+          if (core_service === coreName) {
+            sameTypeServices.push(services_array_Y[i]['services_name_en']);
+          }
+        }
+        return sameTypeServices;
+      }
+
+      repos_name = decodeURIComponent(repos_name);
+      var clicked_service = repos_name.replace('_ja', '');
+      clicked_service = clicked_service.replace('_en', '');
+      var md_array = checkCore(clicked_service);
+      var md_array_modified = [];
+
+      md_array.forEach(function (data) {
+        data = data.replace(/ /g, '_');
+        data = data.replace(/\//g, '_');
+        if (repos_name.slice(-3) === '_ja') {
+          data = data + '_ja';
+        } else if (repos_name.slice(-3) === '_en') {
+          data = data + '_en';
+        }
+        md_array_modified.push(data);
+      });
+      $('.service__wrapper').empty();
+
+      md_array_modified.map((data) => {
+        var url = window.location;
+        var path = url.href.split('/');
+        var file_name = path.pop();
+        var lang = '';
+        if (file_name.match(/services-en\.html/)) {
+          lang = 'en';
+        } else {
+          lang = 'ja';
+        }
+        var repos_name = data + '_' + lang;
+        display_description(repos_name);
+      });
+
+      function display_description(repos_name) {
+        $.ajax({
+          type: 'get',
+          url: './services/' + repos_name + '.md',
+        }).done(function (result) {
+          var arranged_data = '';
+          arranged_data = marked(result);
+          $('.service__wrapper').append(
+            $('<div/>')
+              .attr({
+                class: 'markdown-body',
+              })
+              .html(arranged_data)
+          );
+        });
+      }
+    });
+  }
+
+  //ハッシュ値が変わった時の画面遷移
+  window.addEventListener(
+    'hashchange',
+    function () {
+      if (location.hash === '') {
+        $('.facet_section').removeClass('off');
+        $('.service__wrapper').empty();
+        var containerEl = document.querySelector('.service__wrapper');
+        var mixer = mixitup(containerEl, {
+          multifilter: {
+            enable: true,
+          },
+          debug: {
+            enable: true,
+          },
+        });
+        mixer.destroy();
+        servicesFrontDisplay();
+      } else {
+        var service__title = location.hash.slice(1);
+        $('.facet_section').addClass('off');
+        displayRepos(service__title);
+      }
+    },
+    false
+  );
+});
