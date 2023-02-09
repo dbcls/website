cfceedf2ca80270652d4881d540698159131ba4b
Marco Chan <marcochan94@hotmail.com>
2023-02-10 00:51:00 +0900

    switch lib to swiper

--- script/index.js
+++ script/index.js
@@ -5,50 +5,32 @@ document.head.appendChild(script);
 
 script.addEventListener('load', function () {
   $('.news__individual-wrapper').css('display', 'block');
-  function slideSwitch() {
 
-    var $active = $('.main-image__contents DIV.active');
-    if ($active.length == 0) $active = $('.main-image__contents DIV:last');
-
-    // use this to pull the divs in the order they appear in the markup
-    var $next = $active.next().length
-      ? $active.next()
-      : $('.main-image__contents DIV:first');
-
-    // uncomment below to pull the divs randomly
-    // var $sibs  = $active.siblings();
-    // var rndNum = Math.floor(Math.random() * $sibs.length );
-    // var $next  = $( $sibs[ rndNum ] );
-
-    $active.addClass('last-active');
-
-    $next
-      .css({
-        opacity: 0.0,
-      })
-      .addClass('active')
-      .animate(
-        {
-          opacity: 1.0,
-        },
-        1000,
-        function () {
-          $active.removeClass('active last-active');
-        }
-      );
-  }
-
-  setInterval(function () {
-    slideSwitch();
-  }, 5000);
-
-  // スライダー https://ja.splidejs.com/documents/
-  var splide = new Splide('.splide', {
-    type: 'fade',
-    autoplay: true,
-    rewind: true,
-    interval: 5000,
-    speed: 2000,
+  // Swiper Library https://swiperjs.com/
+  var swiper = new Swiper('.mySwiper', {
+    autoplay: {
+      delay: 2000,
+    },
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesProgress: true,
+    direction: 'vertical',
+    // loopedSlides: 1,
   });
-  splide.mount();
+  var swiper2 = new Swiper('.mySwiper2', {
+    autoplay: {
+      delay: 2000,
+      stopOnLastSlide: true,
+    },
+    // spaceBetween: 10,
+    // loopedSlides: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
 });
