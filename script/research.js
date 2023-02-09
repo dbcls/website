12a45686aad5668df6600f404f10303ee48fe459
Marco Chan <marcochan94@hotmail.com>
2023-02-09 16:09:48 +0900

    add tooltips in modal view

--- script/research.js
+++ script/research.js
@@ -14,6 +14,7 @@ script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
 document.head.appendChild(script);
 
 script.addEventListener('load', function () {
+  const lang = $('html').attr('lang');
   // Modal ViewのON/OFF toggle
   const toggles = $('.modal-toggle');
   const toggleArray = Array.from(toggles).entries();
@@ -33,4 +34,29 @@ script.addEventListener('load', function () {
   //Pageコード直後には animation 無効化
   let node = document.querySelector('.preload-transitions');
   node.classList.remove('preload-transitions');
+
+  $.ajax({
+    url: '../json/services-object.json',
+    dataType: 'json',
+    async: true,
+    success: (data) => {
+      tippy('.tooltip', {
+        content: (el) => {
+          const span = document.createElement('span');
+          for (const [i, item] of data.entries()) {
+            if (item.services_name_en !== el.getAttribute('id')) {
+              continue;
+            }
+            const title = item[`services_name_${lang}`];
+            const introduction = item[`explanation_${lang}`];
+            span.innerHTML = `${title}<br>${introduction}`;
+          }
+          return span;
+        },
+        arrow: false,
+        maxWidth: 220,
+        duration: [300, 250],
+      });
+    },
+  });
 });
