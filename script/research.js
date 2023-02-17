fae51b87911423cd80be98be2f8e015ea0d3026e
Marco Chan <marcochan94@hotmail.com>
2023-02-18 02:22:41 +0900

    refactor research page and delete unused sass

--- script/research.js
+++ script/research.js
@@ -1,7 +1,5 @@
-var script = document.createElement('script');
 var material_icons = document.createElement('link');
 
-script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
 script_sticky.setAttribute('src', '/script/stickyfill.min.js');
 material_icons.setAttribute('rel', 'stylesheet');
 material_icons.setAttribute(
@@ -9,9 +7,6 @@ material_icons.setAttribute(
   'https://fonts.googleapis.com/icon?family=Material+Icons'
 );
 document.head.appendChild(material_icons);
-
-script.setAttribute('src', 'https://code.jquery.com/jquery-3.2.1.min.js');
-document.head.appendChild(script);
 
 script.addEventListener('load', function () {
   const lang = $('html').attr('lang');
